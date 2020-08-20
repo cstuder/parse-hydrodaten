4aad5d5e9dfffed1168c026a2402d5ee5e728802
Christian Studer <cstuder@existenz.ch>
2020-08-20 09:29:45 +0200

    DataParser: Implements working DataParser

--- src/DataParser.php
+++ src/DataParser.php
@@ -7,12 +7,83 @@ namespace cstuder\ParseHydrodaten;
  */
 class DataParser
 {
+    /**
+     * Timezone of the timestamps in the XML string
+     */
+    protected const TIMEZONE = 'Europe/Zurich';
+
+    /**
+     * NULL string as used in the XML string
+     */
+    protected const NULL_STRING = 'NaN';
+
     public static function parse(string $raw)
     {
         $data = [];
+        $xml = simplexml_load_string($raw);
 
-        // TODO implement this
+        // Loop over stations
+        foreach ($xml->station as $station) {
+            // Extract values
+            $loc = (int) $station['number'];
+
+            foreach ($station->parameter as $parameter) {
+                $type = (int) $parameter['type'];
+
+                $datetime = (string) $parameter->datetime;
+                $value = self::parseValue($parameter->value);
+                $previousValue = self::parseValue($parameter->{'previous-24h'});
+
+                // Validate data
+                $timestamp = strtotime($datetime . ' ' . self::TIMEZONE);
+                if ($timestamp == 0) {
+                    continue;
+                }
+
+                // Valid value found
+                if ($value !== NULL) {
+                    $data[] = (object) ([
+                        'timestamp' => $timestamp,
+                        'loc' => $loc,
+                        'par' => $type,
+                        'val' => $value
+                    ]);
+                }
+
+                // Old data found as well
+                if ($previousValue !== NULL) {
+                    $previousTimestamp = strtotime($datetime . ' ' . self::TIMEZONE . ' - 1 day');
+
+                    $data[] = (object) ([
+                        'timestamp' => $previousTimestamp,
+                        'loc' => $loc,
+                        'par' => $type,
+                        'val' => $previousValue
+                    ]);
+                }
+            }
+        }
 
         return $data;
+    }
+
+    /**
+     * Parse Wert tag
+     * 
+     * Recognizes empty strings and returns NULL.
+     * Parses to float otherwise, removing thousands separators
+     * 
+     * @param string $value
+     * @return null|float
+     */
+    public static function parseValue(?string $value)
+    {
+        if ($value === self::NULL_STRING) return null;
+
+        if ($value === '') return null;
+
+        if ($value === null) return null;
+
+        return floatval($value);
     }
 }
--- tests/DataParserTest.php
+++ tests/DataParserTest.php
@@ -30,6 +30,20 @@ class DataParserTest extends TestCase
         $this->assertEquals(34, count($this->collectTimestamps($data)));
     }
 
+    public function testParseValue()
+    {
+        $this->assertNull(\cstuder\ParseHydrodaten\DataParser::parseValue(''));
+        $this->assertNull(\cstuder\ParseHydrodaten\DataParser::parseValue(null));
+        $this->assertNull(\cstuder\ParseHydrodaten\DataParser::parseValue('NaN'));
+
+        $this->assertEquals(0, \cstuder\ParseHydrodaten\DataParser::parseValue('0'));
+        $this->assertEquals(0, \cstuder\ParseHydrodaten\DataParser::parseValue('0.0'));
+        $this->assertEquals(-5, \cstuder\ParseHydrodaten\DataParser::parseValue('-5.0'));
+        $this->assertEquals(2, \cstuder\ParseHydrodaten\DataParser::parseValue('2'));
+        $this->assertEquals(12345, \cstuder\ParseHydrodaten\DataParser::parseValue('12345'));
+        $this->assertEquals(12345.67, \cstuder\ParseHydrodaten\DataParser::parseValue('12345.67'));
+    }
+
     private function collectParameters($data)
     {
         $allParameters = array_map(function ($d) {
